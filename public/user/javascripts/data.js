8003c020475fb15228c7b440a6e82658281f9619
Dylan Conway <funi1234@gmail.com>
2015-07-29 16:24:38 +0100

    code style mistakes

--- public/user/javascripts/data.js
+++ public/user/javascripts/data.js
@@ -1,6 +1,6 @@
 var selectedCloudlet = '';
 
-var typeCache = {}
+var typeCache = {};
 
 
 $('#typesmain ul li a').click(function(){
@@ -9,7 +9,7 @@ $('#typesmain ul li a').click(function(){
    var auth  = $("#session").val();
    var cloud = $("#cloudlet").val();
 
-   $("#displayContainer").hide()
+   $("#displayContainer").hide();
    $("#id_list").html("");
 
    $.ajax({
@@ -28,7 +28,7 @@ $('#typesmain ul li a').click(function(){
          }
 
          updateEntryIds();
-         $("ul#id_list li a").click(display_object_function)
+         $("ul#id_list li a").click(display_object_function);
          clickFirstIfSingle("#id_list li a")
       },
       error: function (data) {
@@ -41,10 +41,10 @@ $('#typesmain ul li a').click(function(){
 
 
 var idToStr = function(id){
-   var hex       = id.substring(1,13).replace('-', '')
-   var timeStamp = parseInt(hex, 16)
-   var date      = new Date(timeStamp)
-   var dateStr   = date.toLocaleString()
+   var hex       = id.substring(1,13).replace('-', '');
+   var timeStamp = parseInt(hex, 16);
+   var date      = new Date(timeStamp);
+   var dateStr   = date.toLocaleString();
 
    return "Entry: " + dateStr.substring(0, dateStr.length -3)
 }
@@ -53,25 +53,25 @@ var idToStr = function(id){
 var updateEntryIds = function(){
    $("#id_list li a").each(function(){
       var elm       = $(this);
-      var id        = elm.attr('name')
-      var display   = idToStr(id)
+      var id        = elm.attr('name');
+      var display   = idToStr(id);
       elm.html(display)
    })
-}
+};
 
 
 var clickFirstIfSingle = function(id){
-   var arr = $(id)
+   var arr = $(id);
    if (1 === arr.length){
       arr[0].click()
    }
-}
+};
 
 
 $(window).load(function(){
    $('#typesmain ul li a').each(function(){
-      var obj    = $(this)
-      var typeId = obj.attr('name')
+      var obj    = $(this);
+      var typeId = obj.attr('name');
 
       if (undefined !== typeCache[typeId]){
          return typeCache[typeId]
@@ -86,27 +86,27 @@ $(window).load(function(){
          },
          dataType: 'json',
          success: function (data) {
-            typeCache[typeId] = data
+            typeCache[typeId] = data;
             obj.html(data['@reference'])
          },
          error: function (data) {
             obj.html(typeId)
          }
       });
-   })
+   });
 
    clickFirstIfSingle('#typesmain ul li a')
-})
+});
 
 
 var typeMemberToContext = function(type){
-   var mapping = {}
+   var mapping = {};
    for (var i = 0; i < type['@context'].length; i++){
-      var ce = type['@context'][i]
+      var ce = type['@context'][i];
       mapping[ce['@property_name']] = ce['@context_id']
    }
    return mapping;
-}
+};
 
 
 var getEditButtonHTML = function(obj_id, app_cid, app_id, perms){
@@ -114,7 +114,7 @@ var getEditButtonHTML = function(obj_id, app_cid, app_id, perms){
    '<button type="button" class="btn editPermsButton" data="' + obj_id + "+" + app_cid + "+" + app_id + '" perms="' +
       perms +
    '" >Modify</button></div>'
-}
+};
 
 
 var display_object_function = function(){
@@ -134,84 +134,84 @@ var display_object_function = function(){
       dataType: 'json',
       success: function (resp) {
          //$("#data").html("asdf")
-         var data  = resp.obj
-         var meta  = resp.meta
-         var perms = resp.perms
+         var data  = resp.obj;
+         var meta  = resp.meta;
+         var perms = resp.perms;
 
 
-         $("#displayContainer").show()
+         $("#displayContainer").show();
 
-         var type         = typeCache[data['@type']]
-         var type_mapping = typeMemberToContext(type)
+         var type         = typeCache[data['@type']];
+         var type_mapping = typeMemberToContext(type);
 
-         $("#displayedEntryTitle").html(type['@reference'] + " (" + idToStr(id) + ")")
-         $("#displayedEntryTitle").attr('name', id)
+         $("#displayedEntryTitle").html(type['@reference'] + " (" + idToStr(id) + ")");
+         $("#displayedEntryTitle").attr('name', id);
 
-         $('#data_table tbody').html('')
-         $('#data_table_meta tbody').html('')
-         $('#data_table_perms tbody').html('')
+         $('#data_table tbody').html('');
+         $('#data_table_meta tbody').html('');
+         $('#data_table_perms tbody').html('');
          for ( var i in data['@data']){
-            var value = JSON.stringify(data['@data'][i])
+            var value = JSON.stringify(data['@data'][i]);
             if (value.indexOf('"') === 0 ){
                value = value.substring(1,value.length-1)
             }
             $('#data_table > tbody:last').append('<tr><td>' + type_mapping[i] +'</td><td>'+ value +'</td></tr>');
          }
-         $('#data_table').bootstrapTable()
+         $('#data_table').bootstrapTable();
 
 
          for ( var i in meta){
             $('#data_table_meta > tbody:last').append('<tr><td>' + meta[i].key +'</td><td>'+ meta[i].value +'</td></tr>');
          }
-         $('#data_table_meta').bootstrapTable()
+         $('#data_table_meta').bootstrapTable();
 
          for ( var i in perms){
             $('#data_table_perms > tbody:last').append('<tr><td>' + perms[i].key +'</td><td>'+ perms[i].value +'</td><td>'+ getEditButtonHTML(id, perms[i].cid, perms[i].id, perms[i].value) +'</td></tr>');
          }
 
-         $('#data_table_perms').bootstrapTable()
+         $('#data_table_perms').bootstrapTable();
 
          $('.editPermsButton').click(function(){
-            var button = $(this)
-            var data   = button.attr('data')
-            var perms  = button.attr('perms')
+            var button = $(this);
+            var data   = button.attr('data');
+            perms  = button.attr('perms');
 
-            var dataP = data.split("+")
+            var dataP = data.split("+");
 
-            var obj_id = dataP[0]
-            var cid_id = dataP[1]
-            var app_id = dataP[2]
+            var obj_id = dataP[0];
+            var cid_id = dataP[1];
+            var app_id = dataP[2];
 
-            var perms = perms.split(", ")
+            perms = perms.split(", ");
 
             //console.log(obj_id, cid_id, app_id)
             //console.log(perms)
 
-            var c = -1 !== $.inArray("create", perms)
-            var r = -1 !== $.inArray("read",   perms)
-            var u = -1 !== $.inArray("update", perms)
-            var d = -1 !== $.inArray("delete", perms)
+            var c = -1 !== $.inArray("create", perms);
+            var r = -1 !== $.inArray("read",   perms);
+            var u = -1 !== $.inArray("update", perms);
+            var d = -1 !== $.inArray("delete", perms);
 
-            var dataC = {ref : obj_id, type : "object", access_level: "APP", access_type: "CREATE"}
-            var dataR = {ref : obj_id, type : "object", access_level: "APP", access_type: "READ"}
-            var dataU = {ref : obj_id, type : "object", access_level: "APP", access_type: "UPDATE"}
-            var dataD = {ref : obj_id, type : "object", access_level: "APP", access_type: "DELETE"}
+            var dataC = {ref : obj_id, type : "object", access_level: "APP", access_type: "CREATE"};
+            var dataR = {ref : obj_id, type : "object", access_level: "APP", access_type: "READ"};
+            var dataU = {ref : obj_id, type : "object", access_level: "APP", access_type: "UPDATE"};
+            var dataD = {ref : obj_id, type : "object", access_level: "APP", access_type: "DELETE"};
 
-            var p = '<ul id="perms_list">'
+            var p = '<ul id="perms_list">';
             p += '<li><input style="list-style:none;" type="checkbox" name="create" value=\'' + JSON.stringify(dataC) + '\'' + ((c) ? ' checked="true" ' : '') + '> create</li>'
             p += '<li><input style="list-style:none;" type="checkbox" name="read"   value=\'' + JSON.stringify(dataR)  + '\'' + ((r) ? ' checked="true" ' : '') + '> read</li>'
             p += '<li><input style="list-style:none;" type="checkbox" name="update" value=\'' + JSON.stringify(dataU)  + '\'' + ((u) ? ' checked="true" ' : '') + '> update</li>'
             p += '<li><input style="list-style:none;" type="checkbox" name="delete" value=\'' + JSON.stringify(dataD)  + '\'' + ((d) ? ' checked="true" ' : '') + '> delete</li>'
-            p += "</ul>"
+            p += "</ul>";
 
-            $('#modifyModal').find('.modal-title').text('Modify Permissions')
+            $('#modifyModal').find('.modal-title').text('Modify Permissions');
             $('#modifyModal').find('.modal-body').html("Modifying permissions for a specific object may negatively effect your " +
-               "user experience with that application. Please continue with caution.<br/><br/><br/>" + p)
-            $('#modifyModal').find('.okay-button').html("Save")
+               "user experience with that application. Please continue with caution.<br/><br/><br/>" + p);
+            $('#modifyModal').find('.okay-button').html("Save");
 
-            $('#modifyModal').attr("cid", cid_id)
-            $('#modifyModal').attr("aid", app_id)
-            $('#modifyModal').attr("oid", obj_id)
+            $('#modifyModal').attr("cid", cid_id);
+            $('#modifyModal').attr("aid", app_id);
+            $('#modifyModal').attr("oid", obj_id);
 
             $('#modifyModal').modal('show');
 
@@ -227,12 +227,12 @@ var display_object_function = function(){
 
 
 $("#deleteEntryButton").click(function(){
-   var objId = $("#displayedEntryTitle").attr('name')
+   var objId = $("#displayedEntryTitle").attr('name');
 
-   $('#exampleModal').find('.modal-title').text('Delete Entry ' + idToStr(objId))
+   $('#exampleModal').find('.modal-title').text('Delete Entry ' + idToStr(objId));
    $('#exampleModal').find('.modal-body').html("Deleting data outside of its application may negatively effect your " +
-      "user experience with that application. Are you sure you want to delete this Data Entry?")
-   $('#exampleModal').find('.okay-button').html("Delete")
+      "user experience with that application. Are you sure you want to delete this Data Entry?");
+   $('#exampleModal').find('.okay-button').html("Delete");
    $('#exampleModal').modal('show');
 })
 
@@ -245,19 +245,19 @@ $('#modifyModal .okay-button').click(function() {
    //var id  = but.attr("key")
    //var cid = but.attr("cid")
 
-   var updatedPerms = []
+   var updatedPerms = [];
 
-   var cid = $('#modifyModal').attr("cid")
-   var aid = $('#modifyModal').attr("aid")
-   var oid = $('#modifyModal').attr("oid")
+   var cid = $('#modifyModal').attr("cid");
+   var aid = $('#modifyModal').attr("aid");
+   var oid = $('#modifyModal').attr("oid");
 
    $("#perms_list input").each(function(){
-      var inp = $(this)
+      var inp = $(this);
       if (inp.prop( "checked")){
-         var p = JSON.parse(inp.val())
+         var p = JSON.parse(inp.val());
          updatedPerms.push(p)
       }
-   })
+   });
 
    var perms = {modify_object : true, permissions : updatedPerms}
 
@@ -279,7 +279,7 @@ $('#modifyModal .okay-button').click(function() {
 
 $('#exampleModal .okay-button').click(function() {
 
-   var objId = $("#displayedEntryTitle").attr('name')
+   var objId = $("#displayedEntryTitle").attr('name');
    var auth  = $("#session").val();
 
    $.ajax({
@@ -292,8 +292,8 @@ $('#exampleModal .okay-button').click(function() {
       },
       dataType: 'json',
       success: function (data) {
-         $("#displayContainer").hide()
-         $("#id_list li a[name='" + objId + "']").parent().remove()
+         $("#displayContainer").hide();
+         $("#id_list li a[name='" + objId + "']").parent().remove();
          $('#exampleModal').modal('hide');
       },
       error: function (data) {
